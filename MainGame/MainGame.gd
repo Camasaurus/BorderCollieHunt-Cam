93fd6306de05eccac01df69d02db6f83fb4fbab3
Cam <0157863@schoolsnet.act.edu.au>
2022-03-22 15:13:33 +1100

    fixed High Score system
    the high score system works by using an array to save the past five runs of scores, and removes the fifth score (technically fourth element) to save newer scores. Now to finalise the tutorial...

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -18,21 +18,20 @@ func _ready():
 	# if currentscore > highscore, then update highscore ?
 	# Store the score in an array.
 	# go to the endscene.
-	
-	GlobalVariables.previousScores.push_front(GlobalVariables.scoringInformation["currentScore"])
+	if GlobalVariables.previousScores.size() < 5:
+		GlobalVariables.previousScores.push_front(GlobalVariables.scoringInformation["currentScore"])
+	else: 
+		GlobalVariables.previousScores.remove(4)
+		# This removes the fourth element of the high scores (0,1,2,3,4) <-
+		# a bug with this is that you need a new run per run to reset for newest score
 	print(GlobalVariables.previousScores)
 	get_tree().change_scene("res://EndScene.tscn")
 	# if size array > 5 delete the last index
 	# if currentScore > highscore then highScore = currentScore
 
-# warning-ignore:unused_argument
 func _process(delta):
 	$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
 # Process runs every frame refresh, which could be 30 fps for some computers. (21/03/22)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta):
-#	pass
-
-
-
